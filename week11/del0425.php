480ceecfc59764d675d67f641087e42d14782783
ChenSz615 <125664335+ChenSz615@users.noreply.github.com>
2024-06-14 17:44:57 +0800

    Update del0425.php

--- week11/del0425.php
+++ week11/del0425.php
@@ -2,9 +2,8 @@
 
 <?php //從資料庫刪除指定紀錄
     
-    $No = $_GET["No"]; // 接受參數
-    echo $No;
-    //htmlspecialchars($sNum) 將字符轉成html實體 
+    $id = $_GET["id"];
+
 
     //連接MySQL資料庫
     $link = @mysqli_connect( //加@->不要顯示錯誤訊息
@@ -13,13 +12,19 @@
         'Hana3215321',  // 密碼
         'imeveningparty');  // 預設使用的資料庫名稱
 
+        if (!$link) {
+            echo("無法開啟資料庫!<br/>");
+        }
+
     //SQL語法
-    $SQL = "DELETE FROM attendee WHERE No = '$No'";
+    $SQL = "DELETE FROM attendee WHERE id = '$id'";
 
     //送出執行SQL查詢
     if($result = mysqli_query($link, $SQL)){
         echo "<br/>刪除成功";
     }
     
-    echo "<br/><a href = 'index0425.php'>查看資料庫資料</a>"
+    echo "<br/><a href = 'index0425.php'>查看資料庫資料</a>";
+
+    mysqli_close($link);
 ?>
