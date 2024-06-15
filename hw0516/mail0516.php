e61e016615c78276d34da05c8a2c231d62f754ff
ChenSz615 <125664335+ChenSz615@users.noreply.github.com>
2024-06-15 15:57:16 +0800

    Add files via upload

---
+++ hw0516/mail0516.php
@@ -0,0 +1,13 @@
+<meta charset = "utf-8">
+
+<form action = "sendmail0516.php" method = "post">
+Uber Eat廣告<br/>
+
+email:<input type="email" name="to"><br/>
+主旨:<input type="text" name="subject"><br/>
+內文:<textarea rows="30" COLS="30"name="content"></textarea>
+<br/>
+
+<input type= "submit">
+
+</form>
