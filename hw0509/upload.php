ee82ce42ca976b64ccf06b134800fbb9bdf8f0c0
ChenSz615 <125664335+ChenSz615@users.noreply.github.com>
2024-06-15 14:22:59 +0800

    Delete hw0509/upload.php

--- hw0509/upload.php
+++
@@ -1,8 +0,0 @@
-<meta charset='utf8'>
-
-<form action ="uploadcheck.php" method="post" enctype="multipart/form-data">
-
-請選擇檔案 <input type="file" name="myfile"><br/>
-
-<input type="submit">
-</form>
